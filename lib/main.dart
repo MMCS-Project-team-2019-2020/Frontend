3c371dc530d197b2b9fd7e9b5680f675c3f24cc3
Sakorni <ssakorni@gmail.com>
2020-03-07 22:27:51 +0300

    init with github

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,9 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App#2',
-      theme: ThemeData(
-        primarySwatch: Colors.purple
-      ),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: MyHomePage(),
     );
   }
@@ -19,55 +18,46 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-      id: "id1", 
-      title: "SomeTitle1", 
-      amount: 69.99, 
-      date: DateTime.now()
-      ),
+        id: "id1", title: "SomeTitle1", amount: 69.99, date: DateTime.now()),
     Transaction(
-      id: "id2", 
-      title: "ElDora", 
-      amount: 01.99, 
-      date: DateTime.now()
-    ),
+        id: "id2", title: "ElDora", amount: 01.99, date: DateTime.now()),
     Transaction(
-      id: "id3", 
-      title: "Some wear", 
-      amount: 432.22, 
-      date: DateTime.now()
-      )
+        id: "id3", title: "Some wear", amount: 432.22, date: DateTime.now()),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Playground"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
+        appBar: AppBar(
+          title: Text("Playground"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
               width: double.infinity,
-            child: Card(color: Colors.purple, 
-              child: Text("Chart"),
-            elevation: 10,),
-          ),
-          Container(
-            child: 
-            Column(children: transactions.map((tx) {
-              return Card(
-                child: Row(
+              child: Card(
+                color: Colors.purple,
+                child: Text("Chart"),
+                elevation: 10,
+              ),
+            ),
+            Container(
+              child: Column(
+                  children: transactions.map((tx) {
+                return Card(
+                    child: Row(
                   children: <Widget>[
                     Container(
-                      margin: EdgeInsets.symmetric(horizontal:30, vertical: 20),
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 20),
                       child: Text('\$' + tx.amount.toString()),
                       decoration: BoxDecoration(
-                         border: Border.all(
-                          color: Colors.black, width:2,
-                        )
-                      ),
+                          border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      )),
                       padding: EdgeInsets.all(5),
                     ),
                     Column(
@@ -77,11 +67,10 @@ class MyHomePage extends StatelessWidget {
                       ],
                     )
                   ],
-                )
-              );
-            }).toList()
-            ),
+                ));
+              }).toList()),
             )
-      ],));
+          ],
+        ));
   }
 }
