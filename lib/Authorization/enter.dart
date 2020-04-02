88dbfe5a231890912ff661b8e7c61567f179932e
Sakorni <ssakorni@gmail.com>
2020-04-02 14:48:02 +0300

    Added class User and building it from http.get request

--- lib/Authorization/enter.dart
+++ lib/Authorization/enter.dart
@@ -1,6 +1,37 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import './login_screen.dart';
+
+class User {
+  int id;
+  String name;
+  String phone;
+  List<int> cards;
+
+  User({this.id, this.name, this.phone, this.cards});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: int.parse(json['id']),
+        name: json['name'],
+        phone: json['phone'],
+        cards: json['own_cards'].map(int.parse).toList());
+  }
+}
+
+Future<User> getProfile(String login) async {
+  String request =
+      "http://www.vvd-rks.ru/proj/?action=get-profile&login=$login";
+  await http.get(request).then((response) {
+    var answer = json.decode(response.body)['response'];
+    return User(
+        id: int.parse((answer['id'])),
+        name: answer['name'],
+        phone: answer['phone'],
+        cards: answer[['own_cards'].map(int.parse).toList()]);
+  });
+}
 
 Future<void> logIn(
   String login,
