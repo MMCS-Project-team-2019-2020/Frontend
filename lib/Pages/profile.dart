aa2ad89e7b8cc0d85738334976348649bd89a85f
Sakorni <ssakorni@gmail.com>
2020-04-02 16:46:39 +0300

    now it downloads data from server. Also changed icon for username

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,40 +1,46 @@
-
 import 'package:flutter/material.dart';
+import 'package:shared/Pages/infocards.dart';
 import 'package:shared/widgets/info_card.dart';
+import '../Authorization/enter.dart';
+import '../Authorization/login_screen.dart';
+
 const url = 'http://vk.com/';
 const email = 'AkzhigitovRifat@gmail.com';
 const phone = '+7928-147-0-371';
+User user;
 
 class GridContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-              'Ваш Профиль',
-              style: TextStyle(
-                fontSize: 60.0,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'мои визитки',
-              style: TextStyle(
-                fontSize: 25.0,
-                color: Colors.teal[50],
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-              width: 200,
-              child: Divider(
-                color: Colors.teal.shade700,
-              ),
-            ),
-            InfoCard(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Ваш Профиль',
+          style: TextStyle(
+            fontSize: 60.0,
+            color: Colors.white,
+          ),
+        ),
+        Text(
+          'мои визитки',
+          style: TextStyle(
+            fontSize: 25.0,
+            color: Colors.teal[50],
+            letterSpacing: 2.5,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+          width: 200,
+          child: Divider(
+            color: Colors.teal.shade700,
+          ),
+        ),
+        ICards(),
+
+        /*InfoCard(
               text: phone,
               icon: Icons.phone,
             ),
@@ -50,11 +56,8 @@ class GridContent extends StatelessWidget {
               text: 'Rostov-on-Don, Russia',
               icon: Icons.location_city,
             ),
-           
-          ],
-        );
-    
+           */
+      ],
+    );
+  }
 }
-
-}
-
