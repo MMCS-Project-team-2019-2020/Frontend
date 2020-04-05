f72cbdc91e77e7f92e26332364f9e624d33377ee
Sakorni <ssakorni@gmail.com>
2020-04-05 12:33:35 +0300

    testfield for changes

--- lib/Pages/account.dart
+++ lib/Pages/account.dart
@@ -1,13 +1,25 @@
-
 import 'package:flutter/material.dart';
+import 'profile.dart';
+import '../Authorization/enter.dart';
+
+String _prof;
+String _id;
 
 class AccountContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-          return Column(
-           
-        );
-    
-}
-
+    return Center(
+        child: Column(
+      children: <Widget>[
+        Text("Test for request!"),
+        RaisedButton(
+          onPressed: () {
+            getCard("1005", "10011", main_user);
+            getProfile(main_user.id, main_user);
+          },
+          child: Text("Testing button!"),
+        ),
+      ],
+    ));
+  }
 }
